f638ffdf4483d38fc8f77f927a558b9647ad1dd1
smmlt <kbodia80@gmail.com>
2026-04-03 17:28:48 +0200

    03.04.2026

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,19 @@
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 import React from 'react';
+import { Tabs } from 'expo-router';
+import { BookOpen, Users, User} from 'lucide-react-native';
 
-export default function _layout() {
+export default function TabsLayout() {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Tabs screenOptions={{ headerShown: true }}>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Classes',
+          tabBarIcon: ({ color }) => <BookOpen color={color} size={24} />
+        }}
+      />      
+    </Tabs>>
   );
 }
 
